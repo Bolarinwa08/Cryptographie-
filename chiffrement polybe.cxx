f5d35f0563c49b4a5af7eb47413f55be54d7d8a5
Mohamed CHITOU <139342758+Bolarinwa08@users.noreply.github.com>
2023-11-28 15:38:32 +0100

    chiffrement polybe.cxx

--- chiffrement polybe.cxx
+++ chiffrement polybe.cxx
@@ -2,9 +2,10 @@
 #include <cctype>
 #include <algorithm>
 
+// Fonction pour créer la grille de substitution en utilisant le mot-clé
 std::string createSubstitutionGrid(const std::string& keyword) {
-     std::string alphabet =
-     "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+    // L'alphabet de base (sans 'J' qui est combiné avec 'I')
+    std::string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
     std::string grid = keyword;
 
     // Supprime les lettres du mot-clé de l'alphabet
@@ -18,22 +19,28 @@ std::string createSubstitutionGrid(const std::string& keyword) {
     return grid;
 }
 
+// Fonction pour chiffrer un message en utilisant le chiffre de Polybe
 std::string polybiusEncrypt(const std::string& message, const std::string& keyword) {
+    // Crée la grille de substitution en utilisant le mot-clé
     std::string grid = createSubstitutionGrid(keyword);
     std::string encryptedMessage = "";
 
     for (char letter : message) {
         if (std::isalpha(letter)) {
             letter = std::toupper(letter);
+
+            // Remplace 'J' par les coordonnées de 'I'
             if (letter == 'J') {
-                encryptedMessage += "24";  // J est remplacé par les coordonnées de I
+                encryptedMessage += "24";
             } else {
+                // Trouve la position de la lettre dans la grille et calcule les coordonnées
                 int position = grid.find(letter) + 1;
                 int row = position / 5 + 1;
                 int col = position % 5 + 1;
                 encryptedMessage += std::to_string(row) + std::to_string(col);
             }
         } else {
+            // Conserve les caractères non alphabétiques inchangés
             encryptedMessage += letter;
         }
     }
@@ -45,15 +52,18 @@ int main() {
     std::string keyword;
     std::string message;
 
-    std::cout << "Enter the keyword: ";
+    // Saisie du mot-clé
+    std::cout << "Entrez le mot-clé : ";
     std::cin >> keyword;
 
-    std::cout << "Enter the message to encrypt: ";
-    std::cin.ignore(); // Clear the input buffer
+    // Saisie du message à chiffrer
+    std::cout << "Entrez le message à chiffrer : ";
+    std::cin.ignore(); // Vide le tampon d'entrée
     std::getline(std::cin, message);
 
+    // Chiffre le message en utilisant le chiffre de Polybe
     std::string encryptedMessage = polybiusEncrypt(message, keyword);
-    std::cout << "Encrypted Message: " << encryptedMessage << std::endl;
+    std::cout << "Message chiffré : " << encryptedMessage << std::endl;
 
     return 0;
 }
